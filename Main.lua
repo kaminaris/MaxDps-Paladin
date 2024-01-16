9256b4638591abb04c6604a2bf01cc9e2a41b883
doadin <tbkizle@gmail.com>
2024-01-16 18:09:12 -0500

    Update Main

--- Main.lua
+++ Main.lua
@@ -1,31 +1,31 @@
-﻿local addonName, addonTable = ...;
-_G[addonName] = addonTable;
+﻿local addonName, addonTable = ...
+_G[addonName] = addonTable
 
 if not MaxDps then return end
 
 --- @type  MaxDps
-local MaxDps = MaxDps;
-local Paladin = MaxDps:NewModule('Paladin');
-addonTable.Paladin = Paladin;
+local MaxDps = MaxDps
+local Paladin = MaxDps:NewModule('Paladin')
+addonTable.Paladin = Paladin
 
 Paladin.spellMeta = {
 	__index = function(t, k)
-		print('Spell Key ' .. k .. ' not found!');
+		print('Spell Key ' .. k .. ' not found!')
 	end
 }
 
 -- additional comment
 function Paladin:Enable()
 	if MaxDps.Spec == 1 then
-		MaxDps.NextSpell = Paladin.Holy;
-		MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Holy');
+		MaxDps.NextSpell = Paladin.Holy
+		MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Holy')
 	elseif MaxDps.Spec == 2 then
-		MaxDps.NextSpell = Paladin.Protection;
-		MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Protection');
+		MaxDps.NextSpell = Paladin.Protection
+		MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Protection')
 	elseif MaxDps.Spec == 3 then
-		MaxDps.NextSpell = Paladin.Retribution;
-		MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Retribution');
+		MaxDps.NextSpell = Paladin.Retribution
+		MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Retribution')
 	end
 
-	return true;
+	return true
 end
