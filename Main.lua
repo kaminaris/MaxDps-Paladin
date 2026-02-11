579236b5075a38e0486c16e55f6014273bc8cdf9
tbkizle <tbkizle@gmail.com>
2026-02-11 18:07:37 -0500

    Update Main

--- Main.lua
+++ Main.lua
@@ -16,25 +16,16 @@ Paladin.spellMeta = {
 
 -- additional comment
 function Paladin:Enable()
-    if MaxDps:IsClassicWow() then
+
+    if MaxDps.Spec == 1 then
+        MaxDps.NextSpell = Paladin.Holy
+        MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Holy', "info")
+    elseif MaxDps.Spec == 2 then
+        MaxDps.NextSpell = Paladin.Protection
+        MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Protection', "info")
+    elseif MaxDps.Spec == 3 then
         MaxDps.NextSpell = Paladin.Retribution
         MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Retribution', "info")
-    else
-        if MaxDps.Spec == 1 then
-            MaxDps.NextSpell = Paladin.Holy
-            MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Holy', "info")
-        elseif MaxDps.Spec == 2 then
-            MaxDps.NextSpell = Paladin.Protection
-            MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Protection', "info")
-        elseif MaxDps.Spec == 3 then
-            MaxDps.NextSpell = Paladin.Retribution
-            MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Retribution', "info")
-        else
-            if MaxDps:IsCataWow() or MaxDps:IsClassicWow() then
-                MaxDps.NextSpell = Paladin.Retribution
-                MaxDps:Print(MaxDps.Colors.Info .. 'Paladin Retribution', "info")
-            end
-        end
     end
 
     return true
